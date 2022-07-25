03030a97e6a03076f6cf09d04481bdd63f992224
nat-asha117 <nat.asha.fabrizio117@gmail.com>
2022-07-25 16:43:25 +0200

    loading

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -25,32 +25,43 @@ from pgmpy.inference import VariableElimination
 from sklearn import svm
 from sklearn.svm import SVC
 
-Cancer = pd.read_csv("C:\\Users\\natax\\PycharmProjects\\pythonProject\\smoking.csv")
+df_smoke = pd.read_csv("C:\\Users\\natax\\PycharmProjects\\pythonProject\\smoking.csv")
 print()
 print()
 print("Benvenuto nel nostro sistema per predire se, presi dei soggetti, essi sono affetti o meno dal Cancro al Seno")
 print()
 print()
-print(Cancer)
+# print(df_smoke)
 print()
 
+# conversione da stringa a intero + pulizia dataframe - POTREBBE NON SERVIRE!
+#df_smoke["gender"] = df_smoke["gender"].replace("M", 0)
+#df_smoke["gender"] = df_smoke["gender"].replace("F", 1)
+
+df_smoke = df_smoke.drop(["ID", "gender", "eyesight(left)", "eyesight(right)", "hearing(left)", "hearing(right)", "AST", "ALT", "Gtp", "oral", "tartar"], axis=1)
+#df_smoke["gender"] = df_smoke ["gender"].astype(int)
+
+print(df_smoke)
+
 # dataset di input eliminando l'ultima colonna in quanto servirà per l'output
-X = Cancer.drop("smoking", axis=1)
-Y = Cancer["smoking"]
+X = df_smoke.drop("smoking", axis=1)
+Y = df_smoke["smoking"]
 
 # BILANCIAMENTO DELLE CLASSI
 # Proporzione dei non malati di tiroide (0) e malati di tiroide (1): [Numero di (non) malati di tiroide/Numero totale di pazienti]
 print()
-print('non presenza fumo:',Cancer.smoking.value_counts()[0], '(% {:.2f})'.format(Cancer.smoking.value_counts()[0] /Cancer.smoking.count() * 100))
-print('presenza fumo:',Cancer.smoking.value_counts()[1], '(% {:.2f})'.format(Cancer.smoking.value_counts()[1] /Cancer.smoking.count() * 100), '\n')
+print('non presenza fumo:',df_smoke.smoking.value_counts()[0], '(% {:.2f})'.format(df_smoke.smoking.value_counts()[0] /df_smoke.smoking.count() * 100))
+print('presenza fumo:',df_smoke.smoking.value_counts()[1], '(% {:.2f})'.format(df_smoke.smoking.value_counts()[1] /df_smoke.smoking.count() * 100), '\n')
 
 # Visualizzazione del grafico
-Cancer['smoking'].value_counts().plot(kind='bar').set_title('smoking')
+#df_smoke['smoking'].value_counts().plot(kind='bar').set_title('smoking')
 plt.show()
 
+# EVALUATION SELECTION: K-FOLD CROSS VALIDATION
+
 # Creazione della feature X e del target y
-X = Cancer.to_numpy()
-y = Cancer["smoking"].to_numpy() # K-Fold Cross Validation
+X = df_smoke.to_numpy()
+y = df_smoke["smoking"].to_numpy() # K-Fold Cross Validation
 
 
 kf = StratifiedKFold(n_splits=5)  # La classe è in squilibrio, quindi utilizzo Stratified K-Fold ???
@@ -95,12 +106,12 @@ for train_index, test_index in kf.split(X, y):
     training_set, testing_set = X[train_index], X[test_index]
 
     # Dati di train
-    data_train = pd.DataFrame(training_set, columns=Cancer.columns)
+    data_train = pd.DataFrame(training_set, columns=df_smoke.columns)
     X_train = data_train.drop("smoking", axis=1)
     y_train = data_train.smoking
 
     # Dati di test
-    data_test = pd.DataFrame(testing_set, columns=Cancer.columns)
+    data_test = pd.DataFrame(testing_set, columns=df_smoke.columns)
     X_test = data_test.drop("smoking", axis=1)
     y_test = data_test.smoking
 
@@ -136,3 +147,72 @@ for train_index, test_index in kf.split(X, y):
     model['SVM']['precision_list'] = (metrics.precision_score(y_test, y_pred_SVM))
     model['SVM']['recall_list'] = (metrics.recall_score(y_test, y_pred_SVM))
     model['SVM']['f1_list'] = (metrics.f1_score(y_test, y_pred_SVM))
+
+
+# Modello di rapporto
+def model_report(model):
+    df_smoke_models = []
+
+    for clf in model:
+        df_smoke_model = pd.DataFrame({'model': [clf],
+                                     'accuracy': [np.mean(model[clf]['accuracy_list'])],
+                                     'precision': [np.mean(model[clf]['precision_list'])],
+                                     'recall': [np.mean(model[clf]['recall_list'])],
+                                     'f1score': [np.mean(model[clf]['f1_list'])]
+                                     })
+
+        df_smoke_models.append(df_smoke_model)
+
+    return df_smoke_models
+
+
+# Visualizzazione della tabella con le metriche
+df_smoke_models_concat = pd.concat(model_report(model), axis=0).reset_index()  # Concatenazione dei modelli
+df_smoke_models_concat = df_smoke_models_concat.drop('index', axis=1)  # Rimozione dell'indice
+print(df_smoke_models_concat)  # Visualizzazione della tabella
+
+
+# VERIFICA DELL'IMPORTANZA DELLE FEATURES
+# Creazione della feature X e del target y
+X = df_smoke.drop('smoking', axis=1)
+y = df_smoke['smoking']
+
+# Classificatore da utilizzare per la ricerca delle feature principali
+rfc = RandomForestClassifier(random_state=42, n_estimators=100)
+rfc_model = rfc.fit(X, y)
+
+# Tracciamento delle feature in base alla loro importanza
+(pd.Series(rfc_model.feature_importances_, index=X.columns)
+    .nlargest(10)  # Numero massimo di feature da visualizzare
+    .plot(kind='barh', figsize=[10,5])  # Tipo di grafico e dimensione
+    .invert_yaxis())  # Assicuro un ordine decrescente
+
+# Visualizzazione del grafico
+plt.title('Top features derived by Random Forest', size=20)
+plt.yticks(size=15)
+plt.show()
+
+
+
+
+# CREAZIONE DELLA RETE BAYESIANA
+# Conversione di tutti i valori all'interno del dataframe in interi
+df_smoke_int = np.array(df_smoke, dtype=int)
+df_smoke = pd.DataFrame(df_smoke_int, columns=df_smoke.columns)
+
+# Creazione della feature X e del target y
+X_train = df_smoke
+y_train = df_smoke["smoking"]
+
+# Creazione della struttura della rete
+k2 = K2Score(X_train)
+hc_k2 = HillClimbSearch(X_train)
+k2_model = hc_k2.estimate(scoring_method=k2)
+
+# Creazione della rete
+bNet = BayesianNetwork(k2_model.edges())
+bNet.fit(df_smoke, estimator=MaximumLikelihoodEstimator)
+
+# Visualizzazione dei nodi e degli archi
+print('\033[1m' + '\nNodi della rete:\n' + '\033[0m', bNet.nodes)
+print('\033[1m' + '\nArchi della rete:\n' + '\033[0m', bNet.edges)
